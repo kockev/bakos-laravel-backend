0c483e71a8033befb76ce381a548220c454bc14f
kockev <kperation@gmail.com>
2025-08-27 13:40:30 +0200

    [] - seeders updated, added default user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Support\Roles;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $user = User::query()
+                    ->updateOrCreate(
+                        ['email' => 'kperation@gmail.com'],
+                        [
+                            'name'     => 'Kevin Admin',
+                            'password' => Hash::make('changeme'),
+                        ]
+                    );
+
+        $role = Role::query()
+                    ->where('name', Roles::ADMIN)
+                    ->first();
+
+        if ($role) {
+            $user->assignRole($role);
+        }
+    }
+}
